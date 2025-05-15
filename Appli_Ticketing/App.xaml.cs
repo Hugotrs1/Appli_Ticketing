a65927be7110758a6a0cfc3f1a61e9aba97fb2e6
Hugotrs1 <trousselhugo@gmail.com>
2025-05-15 21:50:18 +0200

    commit

--- Appli_Ticketing/App.xaml.cs
+++ Appli_Ticketing/App.xaml.cs
@@ -1,13 +1,43 @@
-﻿using System.Windows;
-using Appli_Ticketing.Views;
+﻿using Appli_Ticketing.Views;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Appli_Ticketing
 {
     public partial class App : Application
     {
+        private MediaPlayer _player;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            // Initialise le MediaPlayer
+            _player = new MediaPlayer();
+
+            var path = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Assets","Sounds",
+                "dino_roar.wav");
+
+            if (File.Exists(path))
+            {
+                _player.Open(new Uri(path, UriKind.Absolute));
+
+                _player.MediaEnded += (s, _) =>
+                {
+                    _player.Position = TimeSpan.Zero;
+                    _player.Play();
+                };
+
+                _player.Play();
+            }
+            else
+            {
+                MessageBox.Show($"Fichier audio introuvable : {path}", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
             var loginWindow = new LoginPage();
             loginWindow.Show();
